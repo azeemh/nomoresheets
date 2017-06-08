7db4b2a7e2906ca09591ed2fdda9226a4dd0e1b9
azeemh <azeem.hussein@gmail.com>
2017-06-08 15:12:40 -0400

    updated server ip address

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '34.226.218.4', user: 'deploy', roles: %w{app db web}
+server '34.195.198.104', user: 'deploy', roles: %w{app db web}
 
 
 
